55677c3a916ff2437c0896b645b78a37d3c46c8f
bdpedigo <benjamindpedigo@gmail.com>
2021-04-30 13:12:29 -0400

    update with results

--- scripts/semipar_simulations.py
+++ scripts/semipar_simulations.py
@@ -40,6 +40,9 @@ def stashfig(name, **kwargs):
 #    - $p$ = $Semipar(A_1, A_2)$
 #    - Append p-value $p$ to the distribution of p-values under the null
 #
+# For running semipar, I use $n_{bootstraps} = 200$. This is how many samples are used
+# to compute the null distribution to compute the p-value _for each trial_.
+#
 # The above simulation is run using $ASE$ as the embedding method/test statistic.
 #%%
 
@@ -49,6 +52,7 @@ def run_semipar_experiment(
     directed=True,
     perturbation="none",
     n_verts=64,
+    n_bootstraps=200,
     methods=["ase", "omnibus"],
 ):
     np.random.seed(seed)
@@ -71,30 +75,70 @@ def run_semipar_experiment(
     rows = []
     for method in methods:
         lpt_result = latent_position_test(
-            A1, A2, n_components=1, workers=1, embedding=method, n_bootstraps=200
+            A1,
+            A2,
+            n_components=1,
+            workers=1,
+            embedding=method,
+            n_bootstraps=n_bootstraps,
         )
         lpt_result = lpt_result._asdict()
         lpt_result["method"] = method
         lpt_result["perturbation"] = perturbation
         lpt_result["directed"] = directed
+        lpt_result = {**lpt_result, **lpt_result["misc_stats"]}
+        lpt_result.pop("misc_stats")
+        lpt_result["null_distribution_1"] = list(lpt_result["null_distribution_1"])
+        lpt_result["null_distribution_2"] = list(lpt_result["null_distribution_2"])
         rows.append(lpt_result)
     return rows
 
 
-n_trials = 5
-rng = np.random.default_rng(8888)
-all_rows = []
-seeds = rng.integers(np.iinfo(np.int32).max, size=n_trials)
-all_rows += Parallel(n_jobs=-1, verbose=10)(
-    delayed(run_semipar_experiment)(seed, False) for seed in seeds
-)
-all_rows += Parallel(n_jobs=-2, verbose=10)(
-    delayed(run_semipar_experiment)(seed, True) for seed in seeds
-)
-results = []
-for rows in all_rows:
-    results += rows
-results = pd.DataFrame(results)
+from pkg.io import get_out_dir
+import ast
+
+out_dir = get_out_dir(foldername=foldername)
+
+
+def arrayize(string):
+    return np.array(ast.literal_eval(string), dtype=float)
+
+
+def saveload(name, results):
+    if results is not None:
+        results.to_csv(out_dir / name)
+    results = pd.read_csv(
+        out_dir / name,
+        index_col=0,
+        na_values="",
+        keep_default_na=False,
+        converters=dict(
+            null_distribution_1=arrayize,
+            null_distribution_2=arrayize,
+        ),
+    )
+    return results
+
+
+n_trials = 500
+RECOMPUTE = False
+if RECOMPUTE:
+    rng = np.random.default_rng(8888)
+    all_rows = []
+    seeds = rng.integers(np.iinfo(np.int32).max, size=n_trials)
+    all_rows += Parallel(n_jobs=-2, verbose=10)(
+        delayed(run_semipar_experiment)(seed, False) for seed in seeds
+    )
+    all_rows += Parallel(n_jobs=-2, verbose=10)(
+        delayed(run_semipar_experiment)(seed, True) for seed in seeds
+    )
+    results = []
+    for rows in all_rows:
+        results += rows
+    results = pd.DataFrame(results)
+else:
+    results = None
+results = saveload("null-simulation-results", results)
 
 #%% [markdown]
 # ## Plot ECDFs of p-values under the null
