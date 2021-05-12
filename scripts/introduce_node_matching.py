099fae49afe43fa0bb4b45c174043a6fb75c68f2
bdpedigo <benjamindpedigo@gmail.com>
2021-05-12 14:52:14 -0400

    add node matching expository figure

--- scripts/introduce_node_matching.py
+++ scripts/introduce_node_matching.py
@@ -1,7 +1,7 @@
 #%% [markdown]
-# # Look at pairs
-# Here we just examine several neuron pairs to look at their symmetry in temms of
-# morphology and ipsilateral connection matching.
+# # Node matching
+# Here we briefly explain the idea of using network (or graph) matching to uncover an
+# alignment between the nodes of two networks.
 #%% [markdown]
 # ## Preliminaries
 #%%
@@ -34,6 +34,11 @@ from src.pymaid import start_instance
 from src.visualization import simple_plot_neurons
 from pkg.plot import set_theme
 from graspologic.simulations import er_corr
+import networkx as nx
+from giskard.plot import merge_axes, soft_axis_off
+from matplotlib.colors import ListedColormap
+from graspologic.match import GraphMatch
+from graspologic.plot import heatmap
 
 
 def stashfig(name, **kwargs):
@@ -45,9 +50,10 @@ network_palette, NETWORK_KEY = load_network_palette()
 t0 = time.time()
 set_theme()
 
-#%%
+#%% [markdown]
+# ## Illustrate graph matching on a simulated dataset
 
-np.random.seed(88888)
+#%%
 
 
 def set_light_border(ax):
@@ -57,22 +63,23 @@ def set_light_border(ax):
         ax.spines[side].set_color("lightgrey")
 
 
+# sample two networks
+np.random.seed(88888)
 A1, A2 = er_corr(16, 0.3, 0.9)
-import networkx as nx
 
-from graspologic.plot import heatmap
-
+# set up the plot
 heatmap_kws = dict(cbar=False, center=None)
 fig, axs = plt.subplots(2, 4, figsize=(12, 6))
-ax = axs[0, 0]
-from matplotlib.colors import ListedColormap
 
+# plot graph 1 adjacency
+ax = axs[0, 0]
 colors = ["white", network_palette["Left"]]
 cmap = ListedColormap(colors)
 heatmap(A1, ax=ax, title="Network 1", cmap=cmap, **heatmap_kws)
-ax.set_ylabel("Adjacency")
 set_light_border(ax)
+ax.set_ylabel("Adjacency")
 
+# plot graph 2 adjacency, with a random shuffling of the nodes
 ax = axs[0, 1]
 shuffle_inds = np.random.permutation(len(A2))
 colors = ["white", network_palette["Right"]]
@@ -82,12 +89,15 @@ heatmap(
     ax=ax,
     title="Network 2",
     cmap=cmap,
-    **heatmap_kws
+    **heatmap_kws,
 )
 set_light_border(ax)
 
+# create networkx graphs from the adjacencies
 g1 = nx.from_numpy_array(A1)
 g2 = nx.from_numpy_array(A2)
+
+# plot the first graph as a ball-and-stick
 ax = axs[1, 0]
 pos = nx.kamada_kawai_layout(g1)
 nx.draw_networkx(
@@ -101,11 +111,12 @@ nx.draw_networkx(
 ax.axis("on")
 soft_axis_off(ax)
 ax.set_ylabel("Layout")
+
+# plot the second graph as a ball-and-stick, with it's own predicted layout
 ax = axs[1, 1]
 nx.draw_kamada_kawai(g2, ax=ax, node_color=network_palette["Right"], node_size=100)
 
-from giskard.plot import merge_axes
-
+# draw an arrow in the middle of the plot
 ax = merge_axes(fig, axs, rows=None, cols=2)
 ax.axis("off")
 text = "Network matching"
@@ -126,14 +137,14 @@ ax.text(
     fontsize=60,
 )
 
-from graspologic.match import GraphMatch
-
+# graph match to recover the pairing
 gm = GraphMatch(n_init=30)
 perm_inds = gm.fit_predict(A1, A2)
 ax = axs[0, 3]
 heatmap(A2[perm_inds][:, perm_inds], ax=ax, title="Network 2", cmap=cmap, **heatmap_kws)
 set_light_border(ax)
 
+# plot the second network after alignment, color edges based on matchedness
 edge_colors = []
 edgelist = []
 for edge in g2.edges:
@@ -142,8 +153,6 @@ for edge in g2.edges:
         edge_colors.append("black")
     else:
         edge_colors.append("lightgrey")
-
-
 ax = axs[1, 3]
 nx.draw_networkx(
     g2,
@@ -155,7 +164,14 @@ nx.draw_networkx(
     edgelist=edgelist,
     edge_color=edge_colors,
 )
-from giskard.plot import soft_axis_off
 
 soft_axis_off(ax)
 stashfig("network-matching-explanation")
+
+# %%
+elapsed = time.time() - t0
+delta = datetime.timedelta(seconds=elapsed)
+print("----")
+print(f"Script took {delta}")
+print(f"Completed at {datetime.datetime.now()}")
+print("----")
