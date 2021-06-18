fde8ef61efa14e40b59ef813820e63c3e86ef76a
bdpedigo <benjamindpedigo@gmail.com>
2021-06-18 16:08:30 -0700

    stash changes

--- scripts/latent_distribution_test_soft_omni.py
+++ scripts/latent_distribution_test_soft_omni.py
@@ -1,5 +1,5 @@
 #%% [markdown]
-# # Test for bilateral symmetry - latent distribution test
+# # Test for bilateral symmetry - latent distribution test using soft omni
 #%% [markdown]
 # ## Preliminaries
 #%%
@@ -31,10 +31,16 @@ from pkg.data import (
 from pkg.io import savefig
 from pkg.plot import set_theme
 from giskard.align import joint_procrustes
+from src.visualization import adjplot
+from graspologic.plot import pairplot
+from graspologic.match import GraphMatch
+from graspologic.plot import heatmap
+from sklearn.preprocessing import normalize
+from graspologic.embed import AdjacencySpectralEmbed
 
 
 def stashfig(name, **kwargs):
-    foldername = "latent_distribution_test"
+    foldername = "latent_distribution_test_soft_omni"
     savefig(name, foldername=foldername, **kwargs)
 
 
@@ -99,31 +105,138 @@ def split_adj(adj):
     return ll_adj, rr_adj, lr_adj, rl_adj
 
 
-align_n_components = 24
 preprocess = ["binarize", "rescale"]
 
 raw_adj = mg.sum.adj.copy()
 
 ll_adj, rr_adj, _, _ = split_adj(raw_adj)
 
-ll_adj_to_embed, rr_adj_to_embed = preprocess_for_embed(ll_adj, rr_adj, preprocess)
+ll_adj_to_match, rr_adj_to_match = preprocess_for_embed(ll_adj, rr_adj, preprocess)
 
-X_ll, Y_ll, left_sing_vals, left_elbow_inds = embed(
-    ll_adj_to_embed, n_components=align_n_components
+# ll_adj_to_match = ll_adj_to_match[np.ix_(left_paired_inds, left_paired_inds)]
+# rr_adj_to_match = rr_adj_to_match[
+#     np.ix_(right_paired_inds_shifted, right_paired_inds_shifted)
+# ]
+#%% [markdown]
+# ## Soft matching
+#%%
+gm = GraphMatch(padding="naive", init="barycenter")
+perm_inds = gm.fit_predict(ll_adj_to_match, rr_adj_to_match)
+transport_plan = gm.transport_plan_
+match_plan = np.eye(len(perm_inds))[perm_inds]
+
+#%% [markdown]
+# ### Construct the soft omni matrix
+#%%
+n_left = len(ll_adj_to_match)
+n_right = len(rr_adj_to_match)
+n = max(n_left, n_right)
+
+ll_adj_padded = np.zeros((n, n))
+ll_adj_padded[:n_left, :n_left] = ll_adj_to_match
+
+omni_matrix = np.zeros((2 * n, 2 * n))
+omni_matrix[:n, :n] = ll_adj_padded
+omni_matrix[n:, n:] = match_plan @ rr_adj_to_match @ match_plan.T
+alpha = 0.5
+transport_average = (
+    alpha * ll_adj_padded + alpha * match_plan @ rr_adj_to_match @ match_plan.T
 )
-X_rr, Y_rr, right_sing_vals, right_elbow_inds = embed(
-    rr_adj_to_embed, n_components=align_n_components
-)
+omni_matrix[:n, n:] = transport_average
+omni_matrix[n:, :n] = transport_average
+
+#%% [markdown]
+# ### Embed the soft omni matrix
+align_n_components = 16
+omni = AdjacencySpectralEmbed(n_components=align_n_components, diag_aug=True)
+out_joint_embedding, in_joint_embedding = omni.fit_transform(omni_matrix)
+
+X_ll = out_joint_embedding[:n_left]
+X_rr = out_joint_embedding[n:]
+
+Y_ll = in_joint_embedding[:n_left]
+Y_rr = in_joint_embedding[n:]
+
 
 #%%
+def soft_omni(ll_adj_to_match, rr_adj_to_match, n_components=16):
+    gm = GraphMatch(padding="naive", init="barycenter")
+    perm_inds = gm.fit_predict(ll_adj_to_match, rr_adj_to_match)
+    transport_plan = gm.transport_plan_
+    match_plan = np.eye(len(perm_inds))[perm_inds]
+
+    n_left = len(ll_adj_to_match)
+    n_right = len(rr_adj_to_match)
+    n = max(n_left, n_right)
+
+    ll_adj_padded = np.zeros((n, n))
+    ll_adj_padded[:n_left, :n_left] = ll_adj_to_match
+
+    omni_matrix = np.zeros((2 * n, 2 * n))
+    omni_matrix[:n, :n] = ll_adj_padded
+    omni_matrix[n:, n:] = match_plan @ rr_adj_to_match @ match_plan.T
+    alpha = 0.5
+    transport_average = (
+        alpha * ll_adj_padded + alpha * match_plan @ rr_adj_to_match @ match_plan.T
+    )
+    omni_matrix[:n, n:] = transport_average
+    omni_matrix[n:, :n] = transport_average
+
+    align_n_components = 16
+    omni = AdjacencySpectralEmbed(n_components=align_n_components, diag_aug=True)
+    out_joint_embedding, in_joint_embedding = omni.fit_transform(omni_matrix)
+
+    X_ll = out_joint_embedding[:n_left]
+    X_rr = out_joint_embedding[n:]
+
+    Y_ll = in_joint_embedding[:n_left]
+    Y_rr = in_joint_embedding[n:]
+    return X_ll, Y_ll, X_rr, Y_rr
 
 
-X_ll, Y_ll = joint_procrustes(
-    (X_ll, Y_ll),
-    (X_rr, Y_rr),
-    method="seeded",
-    seeds=(left_paired_inds, right_paired_inds_shifted),
-)
+#%% [markdown]
+### Look at the soft omni embeddings
+
+#%%
+def plot_latents(
+    left,
+    right,
+    title="",
+    n_show=4,
+    alpha=0.6,
+    linewidth=0.4,
+    s=10,
+    connections=False,
+    palette=None,
+):
+    if n_show > left.shape[1]:
+        n_show = left.shape[1]
+    plot_data = np.concatenate([left, right], axis=0)
+    labels = np.array(["Left"] * len(left) + ["Right"] * len(right))
+    pg = pairplot(
+        plot_data[:, :n_show], labels=labels, title=title, size=s, palette=palette
+    )
+    # if connections:
+    #     axs = pg.axes
+    #     for i in range(n_show):
+    #         for j in range(n_show):
+    #             if i != j:
+    #                 ax = axs[i, j]
+    #                 add_connections(
+    #                     left[:, j],
+    #                     right[:, j],
+    #                     left[:, i],
+    #                     right[:, i],
+    #                     ax=ax,
+    #                     alpha=alpha,
+    #                     linewidth=linewidth,
+    #                 )
+    pg._legend.remove()
+    return pg
+
+
+plot_latents(X_ll, X_rr)
+
 
 #%% [markdown]
 # ## Hypothesis testing on the embeddings
@@ -178,8 +291,6 @@ def run_test(
 #%% [markdown]
 # ### Run the two-sample test for varying embedding dimension
 #%%
-
-
 rows = []
 for n_components in np.arange(1, align_n_components + 1):
     left_composite_latent = np.concatenate(
@@ -241,19 +352,19 @@ stashfig(
     f"naive-pvalues-test={test}-n_bootstraps={n_bootstraps}-preprocess={preprocess}"
 )
 
+
 #%% [markdown]
 # ## Now do a synthetic alternative experiment
 #%%
-from sklearn.preprocessing import normalize
 
 align_n_components = 16
 n_perturb = 10
 do_normalize = True
-groups = ["None", "MBONs", "MBINs", "PNs", "KCs", "sensories", "LNs"]
+groups = ["None", "MBONs", "MBINs", "PNs", "KCs", "sensories"]
 rows = []
 for group in groups:
     print(group)
-    ll_adj_perturbed = ll_adj_to_embed.copy()
+    ll_adj_perturbed = ll_adj_to_match.copy()
     if group != "None":
         group_nodes = left_nodes[left_nodes["simple_group"] == group]
         effective_n_perturb = min(n_perturb, len(group_nodes))
@@ -268,23 +379,21 @@ for group in groups:
             ll_adj_perturbed[:, i] = ll_adj_perturbed[perm_inds, i]
     else:
         effective_n_perturb = 0
-    X_ll, Y_ll, left_sing_vals, left_elbow_inds = embed(
-        ll_adj_perturbed, n_components=align_n_components
-    )
-    X_rr, Y_rr, right_sing_vals, right_elbow_inds = embed(
-        rr_adj_to_embed, n_components=align_n_components
+    X_ll, Y_ll, X_rr, Y_rr = soft_omni(
+        ll_adj_perturbed, rr_adj_to_match, n_components=align_n_components
     )
     if do_normalize:
         X_ll = normalize(X_ll)
         Y_ll = normalize(Y_ll)
         X_rr = normalize(X_rr)
         Y_rr = normalize(Y_rr)
-    X_ll, Y_ll = joint_procrustes(
-        (X_ll, Y_ll),
-        (X_rr, Y_rr),
-        method="seeded",
-        seeds=(left_paired_inds, right_paired_inds_shifted),
-    )
+
+    # X_ll, Y_ll = joint_procrustes(
+    #     (X_ll, Y_ll),
+    #     (X_rr, Y_rr),
+    #     method="seeded",
+    #     seeds=(left_paired_inds, right_paired_inds_shifted),
+    # )
 
     for n_components in np.arange(1, align_n_components + 1):
         left_composite_latent = np.concatenate(
@@ -346,140 +455,6 @@ ax.set(xlabel="# of dimensions", ylabel="p-value")
 stashfig(
     f"perturb-p-values-n_perturb={n_perturb}-align_n_component={align_n_components}-normalize={do_normalize}"
 )
-
-#%% [markdown]
-# ## Soft omni
-
-from graspologic.match import GraphMatch
-
-ll_adj_to_match = ll_adj[np.ix_(left_paired_inds, left_paired_inds)]
-rr_adj_to_match = rr_adj[np.ix_(right_paired_inds_shifted, right_paired_inds_shifted)]
-
-gm = GraphMatch(padding="naive", init="barycenter")
-perm_inds = gm.fit_predict(ll_adj_to_match, rr_adj_to_match)
-transport_plan = gm.transport_plan_
-
-(perm_inds == np.arange(len(rr_adj_to_match))).mean()
-
-#%%
-from src.visualization import adjplot
-
-n_look = 200
-perm_inds = gm.perm_inds_
-P = np.eye(len(perm_inds))[perm_inds]
-# transport_plan = P
-# D = transport_plan
-# D = P
-fig, axs = plt.subplots(1, 4, figsize=(20, 5))
-adjplot(ll_adj_to_match[:n_look, :n_look], ax=axs[0], plot_type="scattermap")
-adjplot(rr_adj_to_match[:n_look, :n_look], ax=axs[1], plot_type="scattermap")
-
-right_soft_perm = (D @ rr_adj_to_match @ D.T)[:n_look, :n_look]
-adjplot(right_soft_perm, plot_type="scattermap", ax=axs[2])
-diff = right_soft_perm - ll_adj_to_match[:n_look, :n_look]
-sns.heatmap(
-    diff,
-    ax=axs[3],
-    square=True,
-    cmap="RdBu_r",
-    cbar=False,
-    xticklabels=False,
-    yticklabels=False,
-    center=0,
-)
-axs[3].set_title(np.linalg.norm(diff))
-#%%
-sns.histplot(transport_plan[transport_plan != 0])
-
-#%%
-n_left = len(ll_adj_to_match)
-n_right = len(rr_adj_to_match)
-n = max(n_left, n_right)
-
-ll_adj_padded = np.zeros((n, n))
-ll_adj_padded[:n_left, :n_left] = pass_to_ranks(ll_adj_to_match)
-
-omni_matrix = np.zeros((2 * n, 2 * n))
-omni_matrix[:n, :n] = ll_adj_padded
-omni_matrix[n:, n:] = pass_to_ranks(rr_adj_to_match)
-alpha = 0.5
-transport_average = (
-    alpha * ll_adj_padded
-    + alpha * transport_plan @ pass_to_ranks(rr_adj_to_match) @ transport_plan.T
-)
-omni_matrix[:n, n:] = transport_average
-omni_matrix[n:, :n] = transport_average
-
-from graspologic.plot import heatmap
-
-#%%
-adjplot(omni_matrix, plot_type="scattermap")
-
-#%%
-fig, ax = plt.subplots(1, 1, figsize=(15, 15))
-adjplot(
-    transport_average,
-    plot_type="heatmap",
-    ax=ax,
-    cbar=False,
-)
-
-#%%
-
-from graspologic.embed import AdjacencySpectralEmbed
-
-omni = AdjacencySpectralEmbed(n_components=16, diag_aug=True)
-out_joint_embedding, in_joint_embedding = omni.fit_transform(omni_matrix)
-
-X_ll = out_joint_embedding[:n_left]
-X_rr = out_joint_embedding[n:]
-
-Y_ll = in_joint_embedding[:n_left]
-Y_rr = in_joint_embedding[n:]
-
-#%%
-
-from graspologic.plot import pairplot
-
-
-def plot_latents(
-    left,
-    right,
-    title="",
-    n_show=4,
-    alpha=0.6,
-    linewidth=0.4,
-    s=10,
-    connections=False,
-    palette=None,
-):
-    if n_show > left.shape[1]:
-        n_show = left.shape[1]
-    plot_data = np.concatenate([left, right], axis=0)
-    labels = np.array(["Left"] * len(left) + ["Right"] * len(right))
-    pg = pairplot(
-        plot_data[:, :n_show], labels=labels, title=title, size=s, palette=palette
-    )
-    # if connections:
-    #     axs = pg.axes
-    #     for i in range(n_show):
-    #         for j in range(n_show):
-    #             if i != j:
-    #                 ax = axs[i, j]
-    #                 add_connections(
-    #                     left[:, j],
-    #                     right[:, j],
-    #                     left[:, i],
-    #                     right[:, i],
-    #                     ax=ax,
-    #                     alpha=alpha,
-    #                     linewidth=linewidth,
-    #                 )
-    pg._legend.remove()
-    return pg
-
-
-plot_latents(X_ll, X_rr)
 
 #%% [markdown]
 # ## End
